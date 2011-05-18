1f8dd0de74385573087685c105ee92e58319f67c
Terrence Cui <cuizhenyu007@gmail.com>
2011-05-19 07:52:17 +0800

    update to Rails3.0.5, Terrence

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CLA::Application.config.secret_token = 'b93c4a7fd62d12ca3155c1ee1c0f9e273c8e8ff744d12fc62d1b4c3c4618fcac58698a1ffd0df56a0747f72184efce7dd565830db73cb20b100100aa53d47890'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_newTest_session',
-  :secret      => 'b150f641df9d23354c96b7803f351dc0ea5e8707c225f27f4fcd000cca70e7273b65dd1913021e30cebe65da6cb08aa5475c426e6918696f6935333f1edd0368'
-}
+CLA::Application.config.session_store :cookie_store, :key => '_CLA_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# CLA::Application.config.session_store :active_record_store
